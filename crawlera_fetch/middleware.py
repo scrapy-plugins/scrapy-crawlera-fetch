5eb82d80bc1535a87a82a9c35421771980b5e2ff
Ionut Ciubotariu <ionut.cezar.ciubotariu@gmail.com>
2020-10-24 16:13:25 +0300

    Handle Fetch error response

--- crawlera_fetch/middleware.py
+++ crawlera_fetch/middleware.py
@@ -180,6 +180,24 @@ class CrawleraFetchMiddleware:
                 logger.error(log_msg)
                 return response
 
+        if json_response.get('crawlera_error'):
+            error = json_response["crawlera_error"]
+            message = json_response["body"]
+            self.stats.inc_value("crawlera_fetch/response_error")
+            self.stats.inc_value("crawlera_fetch/response_error/{}".format(error))
+            log_msg = "Error downloading <{} {}> (Original status: {}, Fetch API error message: {})"
+            log_msg = log_msg.format(
+                crawlera_meta["original_request"]["method"],
+                crawlera_meta["original_request"]["url"],
+                json_response["original_status"],
+                message,
+            )
+            if self.raise_on_error:
+                raise CrawleraFetchException(log_msg)
+            else:
+                logger.error(log_msg)
+                return response
+
         self.stats.inc_value(
             "crawlera_fetch/response_status_count/{}".format(json_response["original_status"])
         )
