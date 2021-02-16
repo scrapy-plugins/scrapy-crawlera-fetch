508f395d2ff86da2fb8d21e9726494a5c3bf41d1
Evgeny <evgeny@scrapinghub.com>
2021-02-16 17:16:14 +0300

    Added unit tests for apipass

--- tests/data/__init__.py
+++ tests/data/__init__.py
@@ -2,4 +2,5 @@ SETTINGS = {
     "CRAWLERA_FETCH_ENABLED": True,
     "CRAWLERA_FETCH_URL": "https://example.org",
     "CRAWLERA_FETCH_APIKEY": "secret-key",
+    "CRAWLERA_FETCH_APIPASS": "secret-pass"
 }
